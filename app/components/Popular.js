77e4c1c308aa829ac7a32b103f742dc63b8c5a8b
Mohit Garg <gargmohit@live.in>
2017-04-13 15:52:06 +0530

    popular component refactored

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,12 +1,44 @@
-var React = require('React')
+var React = require('react')
+var PropTypes = require('prop-types')
 
-class Popular extends React.Component {
-  render () {
+function SelectLanguage ({selectedLanguage, onSelect}) {
     var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
     return (
       <ul className='languages'>
-      {languages.map(lang => <li key={lang}>{lang}</li>)}
+        <p>Lang:{selectedLanguage}</p>
+        {languages.map(lang => <li onClick={onSelect.bind(null, lang)}
+          key={lang}
+          style={lang === selectedLanguage ? { color: '#D0021B' } : null}>
+          {lang}
+        </li>
+        )}
       </ul>
+    )
+  }
+
+
+SelectLanguage.propTypes = {
+  selectedLanguage:PropTypes.string.isRequired,
+  onSelect:PropTypes.func.isRequired
+}
+
+class Popular extends React.Component {
+  constructor (props) {
+    super(props)
+    
+    this.state = {
+      selectedLanguage:'All'
+    }
+    this.updateLanguage = this.updateLanguage.bind(this)
+  }
+  updateLanguage(lang) {
+    this.setState({selectedLanguage:lang})
+  }
+  render () {
+    return (
+      <div>
+      <SelectLanguage selectedLanguage={this.state.selectedLanguage} onSelect={this.updateLanguage}/>
+      </div>
     )
   }
 }
