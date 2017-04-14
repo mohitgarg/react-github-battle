250944824216256f681fae1ce7a3c9ee4a258f32
Mohit Garg <gargmohit@live.in>
2017-04-14 21:55:15 +0530

    navbar component added

--- app/components/App.js
+++ app/components/App.js
@@ -1,14 +1,17 @@
 var React = require('react')
 var Popular = require('./Popular')
+var {BrowserRouter, Route} = require('react-router-dom')
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className='container'>
-        <Popular />
-      </div>
-    )
-  }
+    render() {
+        return (
+          <BrowserRouter>
+              <div className='container'>
+                  <Route path="/popular" component={Popular}/>
+              </div>
+          </BrowserRouter>
+        )
+    }
 }
 
 module.exports = App;
---
+++ app/components/Nav.js
@@ -0,0 +1,11 @@
+var React = require('react')
+
+function Nav() {
+    return (
+      <ul className="nav">
+          <li>Home</li>
+          <li>Battle</li>
+          <li>Popular</li>
+      </ul>
+    )
+}
