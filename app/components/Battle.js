11650fe64c4d22084595b576bc4afa24b001f918
Mohit Garg <gargmohit@live.in>
2017-04-15 00:21:41 +0530

    battle state added

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -5,23 +5,40 @@ class PlayerInput extends React.Component {
     constructor (props) {
         super(props)
 
-        this.state = ''
+        this.state = {
+            username : ''
+        }
+
         this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
-    handleChange () {
+    handleChange(e) {
+        var value = e.target.value;
+
+        this.setState({
+            username: value
+        })
     }
-}
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.onSubmit(
+          this.props.id, this.state.username)
+
+    }
     render() {
         return (
-            <form className="column">
+            <form className="column" onSubmit={this.handleSubmit}>
                 <label className="header" htmlFor="username">
                     {this.props.label}
                 </label>
                 <input id='username' placeholder="github username"
                        type="text" autoComplete="off"
                        value = {this.state.username}
-                       onChange={this.handleChange}
+                       onChange={this.handleChange} />
+                <button className="button" type="submit"
+                disabled={!this.state.username}
+                >Submit</button>
             </form>
         )
     }
@@ -45,10 +62,10 @@ class Battle extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this)
     }
     handleSubmit (id, username) {
-        this.setState(() => {
+        this.setState(function () {
             var newState = {}
-            newState[`${id}id`] = username,
-            newState[`${id}Image`] = `http://github.com/${username}.png?size=200`
+            newState[id +'Name'] = username,
+            newState[id+'Image'] = `http://github.com/${username}.png?size=200`
             return newState
         })
     }
